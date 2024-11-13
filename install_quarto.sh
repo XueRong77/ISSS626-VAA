c5becd47d6bea517b4c9144229cea4f6f5adaf38
XueRong77 <81909297+XueRong77@users.noreply.github.com>
2024-11-14 05:05:33 +0800

    Delete install_quarto.sh

--- install_quarto.sh
+++
@@ -1,7 +0,0 @@
-# install_quarto.sh
-#!/bin/bash
-curl -LO https://quarto.org/download/latest/quarto-macos.tar.gz && \
-mkdir -p $HOME/.local && \
-tar -xzf quarto-macos.tar.gz -C $HOME/.local && \
-export PATH=$HOME/.local/quarto/bin:$PATH && \
-quarto render
